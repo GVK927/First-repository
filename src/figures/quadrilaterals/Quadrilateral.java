601cac625fb7f20fa4699fcb2944cf01ded3ef4a
927 <glebkalinin927@gmail.com>
2020-12-08 18:48:46 +0300

    Task 3

---
+++ src/figures/quadrilaterals/Quadrilateral.java
@@ -0,0 +1,15 @@
+package figures.quadrilaterals;
+
+public class Quadrilateral{}
+
+class ConvexQuadrilateral extends Quadrilateral{}
+
+class Trapezoid extends ConvexQuadrilateral{}
+
+class Parallelogram extends Trapezoid{}
+
+class Rectangle extends Parallelogram{}
+
+class Rhombus extends Parallelogram{}
+
+class Square extends Rhombus{}
