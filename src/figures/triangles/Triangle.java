56fcbfdda3be53d17073493d1ae2e214ce9324ce
927 <glebkalinin927@gmail.com>
2020-12-08 18:48:27 +0300

    Task 2

---
+++ src/figures/triangles/Triangle.java
@@ -0,0 +1,8 @@
+package figures.triangles;
+
+public class Triangle{}
+
+class RightTriangle extends Triangle{}
+class IsoscelesTriangle extends Triangle{}
+class EquilateralTriangle extends IsoscelesTriangle{}
+
